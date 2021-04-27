96d90ad2103e1829b6f43451fd76039498a962e6
Joakim Nohlgård <jnohlgard@spotify.com>
2021-04-27 11:51:42 +0200

    Red: Check value at changed position

--- test/GridTest.cpp
+++ test/GridTest.cpp
@@ -13,5 +13,6 @@ TEST_CASE("basic properties") {
 
   CHECK(doors.at({3, 3}) == Door::Closed);
   doors.at({2,2}) = Door::Open;
+  CHECK(doors.at({2, 2}) == Door::Open);
 }
 }  // TEST_SUITE("Grid")
