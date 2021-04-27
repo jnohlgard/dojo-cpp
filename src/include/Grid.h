562e23cebdf8dfd69c4ea15d3dab340dd91dfe21
Joakim Nohlgård <jnohlgard@spotify.com>
2021-04-27 11:58:21 +0200

    Green: Use vector to store multiple values

--- src/include/Grid.h
+++ src/include/Grid.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <utility>
+#include <vector>
 
 /// Dense cartesian 2D grid
 template <typename ValueType>
@@ -8,17 +9,22 @@ class Grid {
   using value_type = ValueType;
   using Vec2 = std::pair<int, int>;
 
-  Grid(Vec2 dimension) : value{} {
+  Grid(Vec2 dimension) : width{dimension.second}, values{index_of(dimension)} {
   }
 
-  const ValueType &at(Vec2) const & {
-    return value;
+  const ValueType &at(Vec2 position) const & {
+    return values.at(index_of(position));
   }
 
-  ValueType &at(Vec2) & {
-    return value;
+  ValueType &at(Vec2 position) & {
+    return values.at(index_of(position));
   }
 
  private:
-  value_type value;
+  std::size_t index_of(const Vec2 &position) {
+    return position.first * width + position.second;
+  }
+
+  int width;
+  std::vector<value_type> values;
 };
