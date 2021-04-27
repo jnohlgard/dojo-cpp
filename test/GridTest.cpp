28df1d4d98fceaad0f86a8c356aa0ae3bbf5319a
Joakim Nohlgård <jnohlgard@spotify.com>
2021-04-27 12:06:35 +0200

    Refactor: Use subcases

--- test/GridTest.cpp
+++ test/GridTest.cpp
@@ -11,9 +11,18 @@ TEST_SUITE("Grid") {
 TEST_CASE("basic properties") {
   Grid<Door> doors{{5, 5}};
 
-  CHECK(doors.at({3, 3}) == Door::Closed);
-  doors.at({2, 2}) = Door::Open;
-  CHECK(doors.at({3, 3}) == Door::Closed);
-  CHECK(doors.at({2, 2}) == Door::Open);
+  SUBCASE("Default constructed values") {
+    for (int x = 0; x < 5; ++x) {
+      for (int y = 0; y < 5; ++y) {
+        CHECK(doors.at({x, y}) == Door::Closed);
+      }
+    }
+  }
+
+  SUBCASE("Assigning values") {
+    doors.at({2, 2}) = Door::Open;
+    CHECK(doors.at({3, 3}) == Door::Closed);
+    CHECK(doors.at({2, 2}) == Door::Open);
+  }
 }
 }  // TEST_SUITE("Grid")
