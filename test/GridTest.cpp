167085ae688814e0f2ed7b90122cca7f9260597b
Joakim Nohlgård <jnohlgard@spotify.com>
2021-04-27 11:58:36 +0200

    Refactor: Apply clang-format

--- test/GridTest.cpp
+++ test/GridTest.cpp
@@ -5,14 +5,14 @@ enum class Door {
   Closed = 0,
   Open,
 };
-//using enum Door; // Not supported in Clang (at least currently)
+// using enum Door; // Not supported in Clang (at least currently)
 
 TEST_SUITE("Grid") {
 TEST_CASE("basic properties") {
   Grid<Door> doors{{5, 5}};
 
   CHECK(doors.at({3, 3}) == Door::Closed);
-  doors.at({2,2}) = Door::Open;
+  doors.at({2, 2}) = Door::Open;
   CHECK(doors.at({3, 3}) == Door::Closed);
   CHECK(doors.at({2, 2}) == Door::Open);
 }
